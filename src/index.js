e0ad872b122b5a9f8c477ec0b308530fd9393071
Ashlee Tan <ashleetanau@gmail.com>
2021-10-01 13:40:51 +1000

    Add Pangea Font 🧸@jakelin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './fonts/Pangea/PangeaTrial-Regular.woff';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
