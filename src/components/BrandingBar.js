a19f2076a59d4d440ed08c2cee4dcd399cc2380c
Ashlee Tan <ashleetanau@gmail.com>
2021-10-02 23:55:56 +1000

    Apply Styled Components to BrandingBar component

--- src/components/BrandingBar.js
+++ src/components/BrandingBar.js
@@ -1,13 +1,27 @@
-import './BrandingBar.css';
+import styled from 'styled-components';
 
-function BrandingBar(props) {
+const Container = styled.div`
+  height: 54px;
+  width: 100%;
+  border-radius: 12px 12px 0 0;
+  display: flex;
+  align-items: center;
+  background-color: ${props => props.backgroundColor};
+`;
+
+const LogoImage = styled.img`
+  margin-left: 12px;
+`;
+
+const BrandingBar = (props) => {
   console.log(props);
   const logoUrl = props.listingCompany.media.logo.templatedUrl.replace('{size}', '170x32');
+
   return (
-    <div className="branding-bar">
-      <img src={logoUrl} alt="" className="logo-image" />
-    </div>
+    <Container backgroundColor={props.listingCompany.branding.primaryColour}>
+      <LogoImage src={logoUrl} alt="" />
+    </Container>
   );
-}
+};
   
 export default BrandingBar;
