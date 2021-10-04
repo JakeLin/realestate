bbb0486e11fe0980a2d95b69a61fe7ef2b74ea7b
Ashlee Tan <ashleetanau@gmail.com>
2021-10-05 00:18:01 +1100

    Add previous and next button to image carousel :bear @jakelin

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,18 +1,71 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+
+import { ChevronLeft, ChevronRight } from '@styled-icons/boxicons-regular';
+
 
 const Container = styled.div`
   height: 480px;
   width: 100%;
+  position: relative;
 `;
 
+const ListingImage = styled.img`
+  position: absolute;
+`;
+
+const ButtonsContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  border: 0px;
+  background-color: transparent;
+`;
 
 const Carousel = (props) => {
+  const chevronSize = 88;
+
   const imageUrls = props.images.map((image) => {
     return image.templatedUrl.replace('{size}', '640x480');
   });
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const previousButtonClicked = () => {
+    setImageIndex((originalIndex) => {
+      if (originalIndex === 0) {
+        return imageUrls.length - 1;
+      }
+      return originalIndex - 1;
+    });
+  };
+
+  const nextButtonClicked = () => {
+    setImageIndex ((originalIndex) =>{
+      if (originalIndex === imageUrls.length - 1) {
+        return 0;
+      }
+      return originalIndex + 1;
+    });
+  };
+
+  
   return (
     <Container>
-      <img src={imageUrls[0]} alt="listing" />
+      <ListingImage src={imageUrls[imageIndex]} alt="listing" />
+      <ButtonsContainer>
+        <Button onClick={previousButtonClicked}>
+          <ChevronLeft size={chevronSize} color="white" />
+        </Button>
+        <Button onClick={nextButtonClicked}>
+          <ChevronRight size={chevronSize} color="white" />
+        </Button>
+      </ButtonsContainer>
     </Container>
   );
 };
