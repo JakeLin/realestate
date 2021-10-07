7ecbabc5de6507f32195ae07ddd38e49c566ddd7
Ashlee Tan <ashleetanau@gmail.com>
2021-10-07 23:57:06 +1100

    Fetch data for PropertyDetails component

--- src/components/PropertyDetail.js
+++ src/components/PropertyDetail.js
@@ -1,7 +1,18 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { getPropertyDetail as get } from '../mockBackend/fetch';
+
 
 const PropertyDetail = () => {
   const { propertyId } = useParams();
+  
+  const [listing, setListing] = useState(null);
+  useEffect(() => {
+    get().then((response) => {
+      setListing(response.data);
+    });
+  }, []);
+  console.log(listing);
   return (
     <div>
       this is property detail page and the id is {propertyId}
