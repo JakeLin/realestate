dd98a5dc6d135d716fbd78f270a9a96654fc94b4
Ashlee Tan <ashleetanau@gmail.com>
2022-01-23 14:39:44 +1100

    style DropDownButton

--- src/components/common/DropDown.tsx
+++ src/components/common/DropDown.tsx
@@ -2,12 +2,21 @@ import styled from "styled-components";
 import { ChevronDown, ChevronUp } from "@styled-icons/boxicons-regular";
 
 const DropDownButton = styled.button `
-
+  border: none;
+  border-bottom-style: dashed;
+  border-bottom-width: 1px;
+  background-color: white;
+  font-family: "PangeaLight";
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 1em;
+  padding: 0;
+  color: rgb(5, 95, 180);
 `;
 
 const DropDown = () => {
   return (
-    <DropDownButton>are a first home buyer <ChevronDown size="24" /></DropDownButton>  
+    <DropDownButton>are a first home buyer<ChevronDown size="20" /></DropDownButton>  
   );
 };
 
